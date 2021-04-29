16edbd70b5a6cd77937cc464303b856616255f99
André Donadon <andre.donadon@outlook.com>
2021-04-29 18:28:36 -0300

    AULA 2 - Exceptions em conversões

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -3,6 +3,10 @@ package br.com.alura.bytebank
 import java.lang.ArithmeticException
 
 fun main() {
+    val entrada: String = "1"
+    val valor: Double = entrada.toDouble()
+
+
     try {
         println("Rodou aqui")
         //10/0
