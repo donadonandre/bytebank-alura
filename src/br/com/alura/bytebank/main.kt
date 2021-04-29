af0e6bbb331d369a6b490fae5d8709842b0bedd4
André Donadon <andre.donadon@outlook.com>
2021-04-29 18:33:03 -0300

    AULA 2 - Utilizando um retorno null dentro do try/catch

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,25 +1,51 @@
 package br.com.alura.bytebank
 
 import java.lang.ArithmeticException
+import java.lang.NumberFormatException
 
 fun main() {
-    val entrada: String = "1"
-    val valor: Double = entrada.toDouble()
-
-
-    try {
-        println("Rodou aqui")
-        //10/0
-        10/2
-    } catch (e: ArithmeticException) {
-        println(e.message)
-        println(e.stackTrace)
-        println(e.cause)
-
-        e.printStackTrace() // Exibe as 3 mensagens acima.
-        println("ATENÇÃO: ArithmeticException foi pegada.")
+    val entrada: String = "1,9"
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch(e: NumberFormatException) {
+        println("Problema na conversão")
+        e.printStackTrace()
+        null
     }
-    finally {
-        println("Isso daqui rodou mesmo assim!")
+
+    if ( valorRecebido != null ) {
+        println("valor recebido $valorRecebido")
     }
+    else {
+        println("valor recebido está nulo")
+    }
+
+    // -----
+    // ou
+    // -----
+
+    val novoValor: Double? = try { entrada.toDouble() } catch(e:NumberFormatException) { null }
+
+    if ( novoValor != null ) {
+        println("novo valor $novoValor")
+    }
+    else {
+        println("novoValor está nulo")
+    }
+
+//    try {
+//        println("Rodou aqui")
+//        //10/0
+//        10/2
+//    } catch (e: ArithmeticException) {
+//        println(e.message)
+//        println(e.stackTrace)
+//        println(e.cause)
+//
+//        e.printStackTrace() // Exibe as 3 mensagens acima.
+//        println("ATENÇÃO: ArithmeticException foi pegada.")
+//    }
+//    finally {
+//        println("Isso daqui rodou mesmo assim!")
+//    }
 }
