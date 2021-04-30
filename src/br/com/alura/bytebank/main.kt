1f3e1561085c73f2fbc0d106adaded9bede8bffb
André Donadon <andre.donadon@outlook.com>
2021-04-30 11:57:56 -0300

    AULA 3 - Refatorando a Exception pra classe e usando ela na prática

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,12 +1,15 @@
 package br.com.alura.bytebank
 
 import br.com.alura.bytebank.exception.SaldoInsuficienteException
+import br.com.alura.bytebank.teste.testaComportamentosConta
 import java.lang.ClassCastException
 
 fun main() {
+    testaComportamentosConta()
+
     for (i in 1..5) {
         println(i)
         val endereco = Any()
-        throw SaldoInsuficienteException()
+        //throw SaldoInsuficienteException()
     }
 }
