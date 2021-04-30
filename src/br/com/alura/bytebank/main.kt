419f27daf2040939fd27d9167dced2d621cc112f
André Donadon <andre.donadon@outlook.com>
2021-04-30 11:51:55 -0300

    AULA 3 - Criando nossa primeira classe de exception

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,11 +1,12 @@
 package br.com.alura.bytebank
 
+import br.com.alura.bytebank.exception.SaldoInsuficienteException
 import java.lang.ClassCastException
 
 fun main() {
     for (i in 1..5) {
         println(i)
         val endereco = Any()
-        throw ClassCastException()
+        throw SaldoInsuficienteException()
     }
 }
