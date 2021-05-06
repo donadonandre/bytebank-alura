57c18fadbc142aba05503f040d60d18d5e91ac36
André Donadon <andre.donadon@outlook.com>
2021-05-06 15:59:52 -0300

    Utilizando o método soma como exemplo, pt-2

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -3,29 +3,33 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
-    val endereco = Endereco(logradouro = "Rua Maestro", numero = 345)
-    val enderecoMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-    println(enderecoMaiusculo)
+//    val endereco = Endereco(logradouro = "Rua Maestro", numero = 345)
+//    val enderecoMaiusculo = "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//    println(enderecoMaiusculo)
+//
+//    // OU
+//
+//    Endereco(logradouro = "Rua Maestro", numero = 345)
+//        .let { endereco ->
+//            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
+//        }
+//        .let(::println)
+//
+//    listOf(
+//        Endereco(),
+//        Endereco(complemento = "apartamento"),
+//        Endereco(complemento = "casa")
+//    )
+//        .filter { endereco -> endereco.complemento.isNotEmpty() }
+//        .let(::println)
 
-    // OU
-
-    Endereco(logradouro = "Rua Maestro", numero = 345)
-        .let { endereco ->
-            "${endereco.logradouro}, ${endereco.numero}".toUpperCase()
-        }
-        .let(::println)
-
-    listOf(
-        Endereco(),
-        Endereco(complemento = "apartamento"),
-        Endereco(complemento = "casa")
-    )
-        .filter { endereco -> endereco.complemento.isNotEmpty() }
-        .let(::println)
-
-    println(soma(1, 5))
+    //println(soma(1, 5))
+    soma(1, 5){
+        println(it)
+    }
 }
 
-fun soma(a: Int, b: Int): Int {
-    return a+b
+fun soma(a: Int, b: Int, resultado:(Int) -> Unit) {
+    println("Soma sendo efetuada")
+    resultado(a+b)
 }
