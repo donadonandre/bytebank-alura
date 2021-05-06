75389e18da58d974dadc27122b7dabbafb16bdad
André Donadon <andre.donadon@outlook.com>
2021-05-06 15:43:49 -0300

    Higher order functions

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -3,14 +3,30 @@ package br.com.alura.bytebank
 import br.com.alura.bytebank.modelo.Endereco
 
 fun main() {
+    val testeFuncao: () -> Unit
+
     Endereco().let {
         it
     }
-    "".let {
-        it
-    }
+    "".let(::testeRecebeString)
+
     1.let {
         it
     }
+    teste({}, 1)
+    outroTeste(1) {}
+}
+
+fun testeRecebeString(valor: String){
+
+}
+
+// higher order function
+fun teste(bloco:() -> Unit, inteiro: Int) {
+
+}
+
+fun outroTeste(inteiro: Int, bloco:() -> Unit) {
+
 }
 
