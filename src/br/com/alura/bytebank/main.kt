b409427898207d4a5318b49402c9ed0cdb086330
André Donadon <andre.donadon@outlook.com>
2021-05-05 23:27:49 -0300

    Isso não é nada funcional

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -1,5 +1,11 @@
 package br.com.alura.bytebank
 
+var numero = 10
+
 fun main() {
+    println(soma(1,5))
+    numero = 3
+    println(soma(1,5))
 }
 
+fun soma(a: Int, b: Int): Int = a + b + numero
