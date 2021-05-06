a7e21c271bdc37032004137dd40146690bbdfbf3
André Donadon <andre.donadon@outlook.com>
2021-05-05 23:48:26 -0300

    Implementando uma variavel que chama uma classe

--- src/br/com/alura/bytebank/main.kt
+++ src/br/com/alura/bytebank/main.kt
@@ -3,9 +3,17 @@ package br.com.alura.bytebank
 fun main() {
     val minhaFuncao: () -> Unit = ::teste
     println(minhaFuncao())
+
+    val minhaFuncaoClasse: () -> Unit = Teste()
+    println(minhaFuncaoClasse())
 }
 
 fun teste() {
     println("executa teste")
 }
 
+class Teste : () -> Unit {
+    override fun invoke() {
+        println("Executa Invoke do Teste")
+    }
+}
