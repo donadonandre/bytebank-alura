f5c8eb089f40b2f3f15df1d3a98afcfb2cd9b57c
André Donadon <andre.donadon@outlook.com>
2021-05-06 22:16:55 -0300

    Scope Functions - Utilizando um receiver na soma

--- src/br/com/alura/bytebank/teste/testaHOF.kt
+++ src/br/com/alura/bytebank/teste/testaHOF.kt
@@ -1,7 +1,12 @@
 package br.com.alura.bytebank.teste
 
 fun testaHOF() {
-    soma(1, 5, resultado = (::println))
+    soma(1, 5, resultado = { valor ->
+        println(valor)
+    })
+    somaReceiver(1, 5, resultado = {
+        println(this)
+    })
 }
 
 fun soma(a: Int, b: Int, resultado:(Int) -> Unit) {
@@ -10,3 +15,9 @@ fun soma(a: Int, b: Int, resultado:(Int) -> Unit) {
     println("Soma feita")
 }
 
+fun somaReceiver(a: Int, b: Int, resultado:Int.() -> Unit) {
+    println("Soma sendo efetuada")
+    val total = a + b
+    total.resultado()
+    println("Soma feita")
+}
